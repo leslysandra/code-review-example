6aae3a1d4e508ee1b399a0e32a6274b1a6382032
Julien Delange <julien@gunnm.org>
2019-08-07 20:24:49 -0700

    Add new code

---
+++ pythoncode2.py
@@ -0,0 +1,54 @@
+def func(arg):
+  print("super arg {0}".format(arg))
+
+def func2(arg):
+  print("super arg {0}".format(arg))
+
+def func3(arg):
+  print("super arg {0}".format(arg))
+
+def func4(arg):
+  print("super arg {0}".format(arg))
+
+def func5(arg):
+  print("super arg {0}".format(arg))
+
+def func6(arg):
+  print("super arg {0}".format(arg))
+
+def func7(arg):
+  print("super arg {0}".format(arg))
+
+def func7(arg, arg2):
+  """
+  blabla
+  :arg: bla
+  """
+  print("super arg {0}".format(arg))
+
+def func8(arg, arg2):
+  """
+  blabla
+  :arg: bla
+  """
+  print("super arg {0}".format(arg))
+
+def func9(arg, arg2):
+  """
+  blabla
+  :arg: bla
+  """
+  print("super arg {0}".format(arg))
+
+def func10(arg, arg2):
+  """
+  blabla
+  :arg: bla
+  """
+  print("super arg {0}".format(arg))
+
+
+
+
+func("bla")
+
