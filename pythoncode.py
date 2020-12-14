f61fb8fbbc9b3e06e382fc4ab809cfc4b2d6f0d0
Julien Delange <julien@gunnm.org>
2020-12-13 17:28:43 -0700

    update code

--- pythoncode.py
+++ pythoncode.py
@@ -1,9 +1,16 @@
+import logging
+
 def func(arg):
   print("super arg {0}".format(arg))
 
 def func2(arg):
   print("super arg {0}".format(arg))
 
+def func3(arg):
+  print("super arg {0}".format(arg))
+  logging.info("super arg {0}".format(arg))
+
 func("bla")
 func2("bla")
+func3("bla")
 
